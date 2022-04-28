c72c04dc5c7c8fc270362791b1d20a1c9f76f2b0
ankitbarela <ankitvish1306@gmail.com>
2022-04-28 08:41:53 +0530

    Added the two components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header-component/>
+    <question-component/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import  HeaderComponent from './components/HeaderComponent.vue'
+import  QuestionComponent from './components/QuestionComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComponent,
+    QuestionComponent
   }
 }
 </script>
---
+++ src/components/QuestionComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Question  component</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'QuestionComponent'
+}
+</script>
