773e9ea8d1f7c0c586f73a412ba0b38a3218c972
ankitbarela <ankitvish1306@gmail.com>
2022-04-28 16:13:26 +0530

    Added the question and answer

--- src/components/QuestionComponent.vue
+++ src/components/QuestionComponent.vue
@@ -2,14 +2,21 @@
   <div class="question-box-container">
     <b-jumbotron>
       <template #lead>
-          {{currentQuestion.question}}
+        {{ currentQuestion.question }}
       </template>
 
       <hr class="my-4" />
 
-      <p>
-        List Of Questions
-      </p>
+      <b-list-group>
+        <b-list-group-item
+          v-for="(answer,index) in answers"
+          :key="answer"
+          @click="selectAnswer(index)"
+          :class="[selectedIndex === index ? 'selected' : '']"
+        >
+          {{ answer }}</b-list-group-item
+        >
+      </b-list-group>
 
       <b-button variant="primary" href="#">Submit</b-button>
       <b-button variant="success" @click="Next" href="#">Next</b-button>
@@ -19,11 +26,64 @@
 
 
 <script>
+import _ from 'lodash'
 export default {
   name: "QuestionComponent",
- props : {
-      currentQuestion : Object,
-      Next : Function
- }
+  props: {
+    currentQuestion: Object,
+    Next: Function,
+  },
+  data(){
+      return {
+          selectedIndex : null,
+          suffledAnswers : []
+      }
+  },
+  watch:{
+      currentQuestion:{
+          immediate : true,
+            handler(){
+                 this.selectedIndex = null
+                 this.suffleAnswer()
+            }
+      }
+    //   currentQuestion(){
+    //       this.selectedIndex = null
+    //       this.suffleAnswer()
+    //   }
+  },
+  methods: {
+    selectAnswer(index) {
+        this.selectedIndex = index
+      console.warn(index)
+    },
+    suffleAnswer(){
+      let answers = [...this.currentQuestion.incorrect_answers ,this.currentQuestion.correct_answer];
+        this.suffledAnswers = _.shuffle(answers)
+    }
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
+    },
+  },
 };
 </script>
+
+<style scoped>
+.list-group-item:hover{
+    background-color: #EEE;
+}
+
+.selected {
+    background-color: lightblue;
+}
+.correct {
+    background-color: yellow;
+}
+.incorrect {
+    background-color: red;
+}
+</style>
